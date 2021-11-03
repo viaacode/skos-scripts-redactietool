9f60a76d7cf3638e538cbf67db88b1f99edc4d4e
Miel Vander Sande <miel.vandersande@meemoo.be>
2021-11-03 16:16:59 +0100

    Adds scripts

--- generate_skos/generate_skos.sh
+++ generate_skos/generate_skos.sh
@@ -4,13 +4,13 @@ tarql=./tarql-1.2/bin/tarql
 rm -rf ./dist
 mkdir ./dist
 
-./lib/normalize.py data/vak.csv Onderwijsniveau Onderwijsgraad Thema > ./dist/vak.csv
-./lib/normalize.py data/thema.csv "Officiële vakkenlijst - koppeling" > ./dist/thema.csv
-./lib/normalize.py data/onderwijsniveau.csv Onderwijsgraad > ./dist/onderwijsniveau.csv
-./lib/normalize.py data/onderwijsgraad.csv Onderwijsniveau > ./dist/onderwijsgraad.csv
+./resources/normalize.py data/vak.csv Onderwijsniveau Onderwijsgraad Thema > ./dist/vak.csv
+./resources/normalize.py data/thema.csv "Officiële vakkenlijst - koppeling" > ./dist/thema.csv
+./resources/normalize.py data/onderwijsniveau.csv Onderwijsgraad > ./dist/onderwijsniveau.csv
+./resources/normalize.py data/onderwijsgraad.csv Onderwijsniveau > ./dist/onderwijsgraad.csv
 
-for f in ./lib/*.sparql; do
+for f in ./resources/*.sparql; do
     $tarql $f > dist/${f/'.sparql'/'.skos.ttl'}
 done
 
-ttl-merge -i ./dist -p ./lib/prefixes.json > dist/merged.skos.ttl
+ttl-merge -i ./dist -p ./resources/prefixes.json > dist/merged.skos.ttl
