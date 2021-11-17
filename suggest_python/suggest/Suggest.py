f478778c453a06f56e6b9697b28a44e2ce203cb6
Miel Vander Sande <miel.vandersande@meemoo.be>
2021-11-17 14:10:39 +0100

    DEV-1820 Adds support for multiple themas and graden

--- suggest_python/suggest/Suggest.py
+++ suggest_python/suggest/Suggest.py
@@ -1,5 +1,5 @@
 import os
-from typing import Collection, Literal
+from typing import List
 
 from SPARQLWrapper import JSON, POST, SPARQLWrapper2
 
@@ -24,7 +24,7 @@ def read_query(file_path):
 class Suggest:
     """A simple api for vocbench data"""
 
-    def __init__(self, endpoint: Literal, user: Literal, password: Literal):
+    def __init__(self, endpoint: str, user: str, password: str):
         self.sparql = SPARQLWrapper2(endpoint)
         self.sparql.setMethod(POST)
         self.sparql.setCredentials(user, password)
@@ -34,10 +34,12 @@ class Suggest:
         self.GET_LIST_QUERY = read_query(query_dir + "get_conceptscheme.sparql")
         self.GET_COLLECTION_QUERY = read_query(query_dir + "get_collection.sparql")
         self.GET_CHILDREN_QUERY = read_query(query_dir + "get_narrower.sparql")
-        self.SUGGEST_BY_LABEL_QUERY = read_query(query_dir + "suggest_by_label.sparql")
-        self.SUGGEST_BY_ID_QUERY = read_query(query_dir + "suggest_by_id.sparql")
+        self.SUGGEST_BY_LABELS_QUERY = read_query(
+            query_dir + "suggest_by_labels.sparql"
+        )
+        self.SUGGEST_BY_IDS_QUERY = read_query(query_dir + "suggest_by_ids.sparql")
 
-    def __exec_query(self, query: Literal, **kwargs):
+    def __exec_query(self, query: str, **kwargs):
         formatted = query.format(**kwargs)
         self.sparql.setQuery(formatted)
         for result in self.sparql.query().bindings:
@@ -47,19 +49,19 @@ class Suggest:
                 "definition": result["definition"].value,
             }
 
-    def get_list(self, scheme: Literal):
+    def get_list(self, scheme: str):
         """Get a thesaurus concepts by scheme id."""
 
         for res in self.__exec_query(self.GET_LIST_QUERY, scheme=scheme):
             yield res
 
-    def get_collection(self, collection: Literal):
+    def get_collection(self, collection: str):
         """Get a collection members by collection id."""
 
         for res in self.__exec_query(self.GET_COLLECTION_QUERY, collection=collection):
             yield res
 
-    def get_children(self, concept: Literal):
+    def get_children(self, concept: str):
         """Get the children of a concept."""
 
         for res in self.__exec_query(self.GET_CHILDREN_QUERY, concept=concept):
@@ -89,18 +91,24 @@ class Suggest:
         for res in self.get_collection(OND_NS + "niveau"):
             yield res
 
-    def suggest(self, thema: Literal, graad: Literal):
+    def suggest(self, thema: List[str], graad: List[str]):
         """Suggest 'vakken' based on the identifiers of 'onderwijsgraad' and 'thema'."""
 
+        themas = ", ".join("<" + str(t) + ">" for t in thema)
+        graden = ", ".join("<" + str(g) + ">" for g in graad)
+
         for res in self.__exec_query(
-            self.SUGGEST_BY_ID_QUERY, thema=thema, graad=graad
+            self.SUGGEST_BY_IDS_QUERY, themas=themas, graden=graden
         ):
             yield res
 
-    def suggest_by_label(self, thema: Literal, graad: Literal):
+    def suggest_by_label(self, thema: List[str], graad: List[str]):
         """Suggest 'vakken' based on 'onderwijsgraad' and 'thema'."""
 
+        themas = ", ".join('"' + str(t) + '"' for t in thema)
+        graden = ", ".join('"' + str(g) + '"' for g in graad)
+
         for res in self.__exec_query(
-            self.SUGGEST_BY_LABEL_QUERY, thema=thema, graad=graad
+            self.SUGGEST_BY_LABELS_QUERY, themas=themas, graden=graden
         ):
             yield res
