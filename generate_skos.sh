08200e04ca5e07a4aaf82de5a5d9885c8d5c2ad0
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-04-06 10:43:44 +0200

    Update scripts to last features

--- generate_skos.sh
+++ generate_skos.sh
@@ -12,7 +12,7 @@ $tarql -H -e "utf-8" ./resources/vak.sparql ./dist/vak-normalized.csv  > ./dist/
 $tarql -H -e "utf-8" ./resources/vak-related.sparql ./dist/vak-normalized.csv  > ./dist/vak-related.skos.ttl
 
 # Create OrderedCollection
-./resources/collect.py ./data/vak.csv Vak https://data.hetarchief.be/id/onderwijs/collectie/vak --ordered --ns https://data.hetarchief.be/id/onderwijs/vak/ --label "vakken" > ./dist/vak-collection.skos.ttl
+./resources/collect.py ./data/vak.csv Vak https://w3id.org/onderwijs-vlaanderen/id/collectie/vak --ordered --ns https://w3id.org/onderwijs-vlaanderen/id/vak/ --label "vakken" > ./dist/vak-collection.skos.ttl
 
 # merge the files
 ttl-merge -i ./dist/vak.skos.ttl ./dist/vak-collection.skos.ttl -p ./resources/prefixes.json > ./dist/vak-norelated-merged.skos.ttl
@@ -39,24 +39,14 @@ skosify ./dist/thema-merged.skos.ttl -o ./dist/thema-final.skos.ttl
 
 # --- Onderwijsstructuur ---
 
-# -> old data <-
-# ./resources/normalize.py data/onderwijsniveau.csv Onderwijsgraad > ./dist/onderwijsniveau.csv
-# $tarql -H -e "utf-8" ./dist/onderwijsniveau.sparql ./dist/onderwijsniveau.csv  > ./dist/onderwijsniveau.skos.ttl
-
-# ./resources/normalize.py data/onderwijsgraad.csv Onderwijsniveau > ./dist/onderwijsgraad.csv
-# $tarql -H -e "utf-8" ./dist/onderwijsgraad.sparql ./dist/onderwijsgraad.csv  > ./dist/onderwijsgraad.skos.ttl
-
-# ttl-merge -i ./dist/onderwijsgraad.skos.ttl ./dist/onderwijsniveau.skos.ttl -p ./resources/prefixes.json > dist/onderwijsstructuur.skos.ttl
-# skosify ./dist/onderwijsstructuur.skos.ttl -o ./dist/onderwijsstructuur-final.skos.ttl 
-
 $tarql -H -e "utf-8" ./resources/onderwijsstructuur.sparql ./data/onderwijsstructuur.csv  > ./dist/onderwijsstructuur.skos.ttl
 
 # Create Collection
-./resources/collect.py ./data/onderwijsstructuur.csv Onderwijsniveau https://data.hetarchief.be/id/onderwijs/collectie/niveau --ns https://data.hetarchief.be/id/onderwijs/structuur/ --label "onderwijsniveaus" > ./dist/niveau-collection.skos.ttl
-./resources/collect.py ./data/onderwijsstructuur.csv Onderwijssubniveau https://data.hetarchief.be/id/onderwijs/collectie/subniveau --ns https://data.hetarchief.be/id/onderwijs/structuur/ --label "onderwijs subniveaus" > ./dist/subniveau-collection.skos.ttl
+./resources/collect.py ./data/onderwijsstructuur.csv Onderwijsniveau https://w3id.org/onderwijs-vlaanderen/id/collectie/niveau --ns https://w3id.org/onderwijs-vlaanderen/id/structuur/ --label "onderwijsniveaus" > ./dist/niveau-collection.skos.ttl
+./resources/collect.py ./data/onderwijsstructuur.csv Onderwijssubniveau https://w3id.org/onderwijs-vlaanderen/id/collectie/subniveau --ns https://w3id.org/onderwijs-vlaanderen/id/structuur/ --label "onderwijs subniveaus" > ./dist/subniveau-collection.skos.ttl
 # Create OrderedCollection
-./resources/collect.py ./data/onderwijsstructuur.csv Onderwijsgraad https://data.hetarchief.be/id/onderwijs/collectie/graad --ordered --ns https://data.hetarchief.be/id/onderwijs/structuur/ --label "onderwijsgraden" > ./dist/graad-collection.skos.ttl
-./resources/collect.py ./data/onderwijsstructuur.csv Leerjaar https://data.hetarchief.be/id/onderwijs/collectie/leerjaar --ordered --ns https://data.hetarchief.be/id/onderwijs/structuur/ --label "onderwijs leerjaren" > ./dist/leerjaar-collection.skos.ttl
+./resources/collect.py ./data/onderwijsstructuur.csv Onderwijsgraad https://w3id.org/onderwijs-vlaanderen/id/collectie/graad --ordered --ns https://w3id.org/onderwijs-vlaanderen/id/structuur/ --label "onderwijsgraden" > ./dist/graad-collection.skos.ttl
+./resources/collect.py ./data/onderwijsstructuur.csv Leerjaar https://w3id.org/onderwijs-vlaanderen/id/collectie/leerjaar --ordered --ns https://w3id.org/onderwijs-vlaanderen/id/structuur/ --label "onderwijs leerjaren" > ./dist/leerjaar-collection.skos.ttl
 
 # merge the files
 ttl-merge -i ./dist/onderwijsstructuur.skos.ttl ./dist/niveau-collection.skos.ttl ./dist/subniveau-collection.skos.ttl ./dist/graad-collection.skos.ttl ./dist/leerjaar-collection.skos.ttl -p ./resources/prefixes.json > dist/onderwijsstructuur-merged.skos.ttl
